492d7986e0043e54b71f4aefad598dc1f9254064
Joseph Moore <josephmoore@Josephs-MacBook-Pro.local>
2018-04-13 18:20:15 -0700

    added ingredients to the recipe

--- prj-start/src/app/shopping-list/shopping-list.component.ts
+++ prj-start/src/app/shopping-list/shopping-list.component.ts
@@ -15,8 +15,10 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-  }
-  onIngredientAdded(ingredient: Ingredient){
-    this.ingredients.push(ingredient);
+    this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    )
   }
 }
