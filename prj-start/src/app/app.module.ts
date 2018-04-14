6e75e9755030834a8ddb1213ddfba92a532946d4
Joseph Moore <josephmoore@Josephs-MacBook-Pro.local>
2018-04-13 17:34:53 -0700

    added a dropdown directive for the dropdowns

--- prj-start/src/app/app.module.ts
+++ prj-start/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { DropDownDirective } from './shared/dropdown-directive';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    DropDownDirective,
   ],
   imports: [
     BrowserModule,
