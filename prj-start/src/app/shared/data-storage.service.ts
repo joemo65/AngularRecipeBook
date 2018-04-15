92b70dba81f55c7492f220169ee15c1728af0261
Joseph Moore <josephmoore@Josephs-MacBook-Pro.local>
2018-04-15 08:10:02 -0700

    changed the http to use the new HttpClient

--- prj-start/src/app/shared/data-storage.service.ts
+++ prj-start/src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
 import { Recipe } from '../recipes/recipe.model';
@@ -8,21 +8,21 @@ import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http,
+    constructor(private httpClient: HttpClient,
                 private recipeService: RecipeService,
                 private authService: AuthService){}
 
     storeRecipes() {
         const token = this.authService.getToken();
-        return this.http.put("https://ng-recipe-book-37aa1.firebaseio.com/recipes.json?auth=" + token, this.recipeService.getRecipes());
+        return this.httpClient.put("https://ng-recipe-book-37aa1.firebaseio.com/recipes.json?auth=" + token, this.recipeService.getRecipes());
     }
 
     getRecipes() {
         const token = this.authService.getToken();
-        this.http.get("https://ng-recipe-book-37aa1.firebaseio.com/recipes.json?auth=" + token)
+        //this.httpClient.get<Recipe[]>("https://ng-recipe-book-37aa1.firebaseio.com/recipes.json?auth=" + token)
+        this.httpClient.get<Recipe[]>("https://ng-recipe-book-37aa1.firebaseio.com/recipes.json?auth=" + token)
             .map(
-                (response: Response) => {
-                    const recipes: Recipe[] = response.json();
+                (recipes) => {
                     for (let recipe of recipes) {
                         if(!recipe['ingredients']) {
                             console.log(recipe);
